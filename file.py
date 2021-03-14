501c4e9db5dcd0bb801207d3639d6c6e82e0bcc4
Colin Griffiths <c1griffi@ucsd.edu>
2021-03-13 19:30:12 -0800

    new adds for python

---
+++ file.py
@@ -0,0 +1,11 @@
+print('Hello World')
+
+print('What is your favorite number?')
+favoriteNum = int(input())
+
+if (favoriteNum <= 10):
+    print('Your favorite number is less than or equal to 10.  Your favorite number is ' + (str(int(favoriteNum))) + '.')
+
+else:
+    print('Your favorite nubmer is greater than 10.  Your favorite number is ' + (str(int(favoriteNum))) + '.')
+
